ca167855d5d506e42a05551ac479c1c5c0aca970
Peter Westling <peter.westling@vermiculus.se>
2024-11-28 13:33:42 +0100

    Add function for creating PR comment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
+import * as core from '@actions/core';
+import { getOctokit, context } from "@actions/github";
+
+async function createPullRequestComment(octokit, repository, pullRequest, comment) {
+  await octokit.rest.issues.createComment({
+    owner: repository.owner.login,
+    repo: repository.name,
+    issue_number: pullRequest.number,
+    body: comment
+  });
+}
+
 async function main() {
   console.log(`Pull request reviewer`);
+  const octokit = getOctokit(process.env.GITHUB_TOKEN);
+  const pullRequest = context.payload.pull_request;
+  const repository = context.payload.repository; 
+
+  if (!pullRequest) {
+    core.setFailed("❌ Expecting pull_request.");
+    return;
+  }
+  if (!repository) {
+    core.setFailed("❌ Expecting repository.");
+    return;
+  }
+
+  await createPullRequestComment(octokit, repository, pullRequest, "PR comment from GitHub action");
 }
 
 main().catch((error) => {
